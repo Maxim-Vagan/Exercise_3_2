89916ac0c191f6872efbe93173b21dbf1d1d41c1
Maxim_Vagan <maxut@gmail.com>
2022-09-15 15:19:18 +0300

    ДЗ 4.2 - Рефакторинг: Доработка скриптов script421.sql - изменение полей таблиц вместо создания таблиц

--- scripts421.sql
+++ scripts421.sql
@@ -1,17 +1,9 @@
 /* Шаг 1 */
-CREATE TABLE student(
-	studentid BIGSERIAL PRIMARY KEY,
-	name VARCHAR(255) UNIQUE NOT NULL,
-	age INTEGER CHECK (age > 16) DEFAULT 20,
-	faculty_id INTEGER,
-	CONSTRAINT faculty_id_fkey FOREIGN KEY (faculty_id)
-		REFERENCES faculty (facultyid) MATCH SIMPLE
-		ON UPDATE NO ACTION ON DELETE NO ACTION
-);
+ALTER TABLE student
+    ADD CONSTRAINT age_const1 CHECK (age > 16),
+    ADD CONSTRAINT name_const1 UNIQUE (name),
+    ADD CONSTRAINT name_const2 CHECK (name IS NOT NULL),
+    ALTER COLUMN age SET DEFAULT 20;
 
-CREATE TABLE faculty(
-	facultyid BIGSERIAL PRIMARY KEY,
-	name VARCHAR(255),
-	color VARCHAR(255),
-	CONSTRAINT pair_const1 UNIQUE (name, color)
-);
+ALTER TABLE faculty
+    ADD CONSTRAINT pair_const1 UNIQUE (name, color);
