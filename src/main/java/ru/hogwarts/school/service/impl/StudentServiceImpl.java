4b442daf53eda7b476118a6c762547fdc929643a
Maxim_Vagan <maxut@gmail.com>
2022-09-22 03:31:38 +0300

    ДЗ 4.4 - Работа с логированием и Профилями

--- src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
+++ src/main/java/ru/hogwarts/school/service/impl/StudentServiceImpl.java
@@ -1,5 +1,7 @@
 package ru.hogwarts.school.service.impl;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 import ru.hogwarts.school.model.Faculty;
 import ru.hogwarts.school.model.Student;
@@ -12,7 +14,7 @@ import java.util.List;
 public class StudentServiceImpl implements StudentService {
 
     private final StudentRepository studRepo;
-
+    private Logger studLogger = LoggerFactory.getLogger(StudentServiceImpl.class);
     public StudentServiceImpl(StudentRepository studRepo) {
         this.studRepo = studRepo;
     }
@@ -20,56 +22,70 @@ public class StudentServiceImpl implements StudentService {
     // Create
     @Override
     public Student addStudent(Student inpStudent) {
+        studLogger.debug("Вызван метод addStudent с inpStudent = " + inpStudent.getName());
         return studRepo.save(inpStudent);
     }
     // Read
     @Override
     public Student findStudent(long inpId) {
+        studLogger.debug("Вызван метод findStudent с inpId = " + inpId);
         return studRepo.findById(inpId).orElse(null);
     }
     // Read
     @Override
     public List<Student> getAllStudents() {
+        studLogger.debug("Вызван метод getAllStudents");
         return studRepo.findAll();
     }
     // Read
     @Override
     public List<Student> getStudentsByAge(int inpAge) {
+        studLogger.debug("Вызван метод getStudentsByAge с inpAge = " + inpAge);
         return studRepo.findByAge(inpAge);
     }
     // Read
     @Override
     public List<Student> getStudentsBetweenAges(int minAge, int maxAge) {
+        studLogger.debug("Вызван метод getStudentsBetweenAges с minAge = " + minAge + ", maxAge = " + maxAge);
         return studRepo.findStudentsByAgeBetween (minAge, maxAge);
     }
     // Read
     @Override
     public Faculty getFacultyOfStudent(long inpId) {
+        studLogger.debug("Вызван метод getFacultyOfStudent с inpId = " + inpId);
         return findStudent(inpId).getFacultyOfStudent();
     }
     // Update
     @Override
     public Student updateStudent(Student inpUpdatedStudent) {
+        Student updatedFaculty = findStudent(inpUpdatedStudent.getStudentid());
+        updatedFaculty.setName(inpUpdatedStudent.getName());
+        updatedFaculty.setAge(inpUpdatedStudent.getAge());
+        studLogger.debug("Вызван метод updateStudent с inpUpdatedStudent.getStudentid = " + inpUpdatedStudent.getStudentid());
         return studRepo.save(inpUpdatedStudent);
     }
     // Delete
     @Override
     public void deleteStudent(long inpId) {
+        studLogger.debug("Удаление данных УЗ студента с ID = " + inpId);
         studRepo.deleteById(inpId);
     }
     // Read
     @Override
     public Integer getAllStudentsByQuery(){
+        studLogger.debug("Вызван метод getAllStudentsByQuery");
         return studRepo.getAllStudentsOfSchool();
     }
     // Read
     @Override
-    public Integer getAvgAgeByQuery(){
+    public Float getAvgAgeByQuery(){
+        studLogger.debug("Вызван метод getAvgAgeByQuery");
         return studRepo.getAvgAgeOfStudents();
     }
     // Read
     @Override
     public List<Student> getLast5Students() {
+        studLogger.debug("Вызван метод getLast5Students");
         return studRepo.getLast5Students();
     }
 }
